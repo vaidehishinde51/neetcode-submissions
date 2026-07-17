aac20dd324454b9aa9e4d28db3f42c2053494754
Vaidehi Shinde <vaidehishinde51@gmail.com>
2026-07-17 20:47:58 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        unordered_map<char,int> freq;
+        
+        for(char c:s){
+            freq[c]++;
+        }
+        for(char c:t){
+            freq[c]--;
+        }
+        for(auto pair:freq){
+            if(pair.second!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};

01bac7e4f5d9051f704897c051e5e9ac3c8081ef
Vaidehi Shinde <vaidehishinde51@gmail.com>
2026-07-24 17:01:04 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string encoded="";
+        for(string &s:strs){
+            encoded+=to_string(s.size())+"#"+s;
+        }
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> result;
+        int i=0;
+        while(i<s.size()){
+            int j=i;
+            while(s[j]!='#'){
+                j++;
+            }
+            int len=stoi(s.substr(i,j-i));
+            result.push_back(s.substr(j+1,len));
+            i=j+1+len;
+        }
+        return result;
+    }
+};
